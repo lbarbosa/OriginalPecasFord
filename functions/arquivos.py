1cd850770cbe1d8cdb8d4a01fd017b9a1317d20c
Lourenço Madruga <lourenco.barbosa.ti@gmail.com>
2022-06-19 00:39:45 -0300

    Update in program

--- functions/arquivos.py
+++ functions/arquivos.py
@@ -1,3 +1,9 @@
+'''
+Graphical User Interface for file processing
+Autor: Lourenço Madruga Barbosa
+
+Funcionalidades da versão: 1.0.0
+'''
 import chardet
 
 
---
+++ windowStart/pageStart.py
@@ -0,0 +1,6 @@
+'''
+Graphical User Interface for file processing
+Autor: Lourenço Madruga Barbosa
+
+Funcionalidades da versão: 1.0.0
+'''
